e6e8e529dc2d82bdf9c0f85adb258e15484cce2b
tbcodes <soypacos@hotmail.com>
2019-05-01 13:39:40 +0200

    Just adding 'for' and 'forEach' code examples!

---
+++ sourceCode.js
@@ -0,0 +1,9 @@
+let colors = ['Grey', 'White', 'Pink', 'Yellow', 'Cyan', 'Tomato']
+
+for (let i = 0; i < colors.length; i++) {
+  console.log (colors[i]);
+}
+
+colors.forEach(function(mycolor) {
+  console.log(mycolor);
+});
